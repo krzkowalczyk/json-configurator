7f8ca772fe2364c96883287998780e979528eb9c
Krzysiek <krz.kowalczyk@gmail.com>
2017-01-17 22:51:44 +0100

    coding

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,23 @@
-input message: '', parameters: [choice(choices: '1.0\n1.1\n1.2\n1.3', description: '', name: 'fnatagent')]
+import groovy.json.JsonSlurperClassic
+
+@NonCPS
+def jsonParse(def json) {
+    new groovy.json.JsonSlurperClassic().parseText(json)
+}
+
+repository['url'] = 'https://github.com/krzkowalczyk/json-configurator.git'
+repository['branch'] = 'devel'
+json_file = './fnetagent.json'
 
 
 node {
-   echo 'Hello World'
+  git branch: repository['branch'], url: repository['url']
+  echo 'Hello World'
+
+    def config =  jsonParse(readFile("config.json"))
+    echo config.key.inspect()
+    def version = config["iis_app"]["fnetagent"]
+
 }
-ś
+
+input message: '', parameters: [choice(choices: '1.0\n1.1\n1.2\n1.3', description: '', name: 'fnetagent')]
